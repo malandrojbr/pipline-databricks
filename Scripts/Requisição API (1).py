7d8abe0d11aae2144a5d42a6139bfccc9c9f9e8f
Jean J <101023567+malandrojbr@users.noreply.github.com>
2023-10-10 23:28:11 -0300

    Add files via upload

---
+++ Scripts/Requisição API (1).py
@@ -0,0 +1,76 @@
+# Databricks notebook source
+# MAGIC %md ## INSTALANDO AS BIBLIOTECAS
+
+# COMMAND ----------
+
+!pip install pandablob -q
+import pandas as pd
+import requests
+import numpy as np
+import pandablob
+from azure.storage.blob import ContainerClient
+
+# COMMAND ----------
+
+# MAGIC %md ## REQUISIÇÃO DA API
+
+# COMMAND ----------
+
+link2 = "https://olinda.bcb.gov.br/olinda/servico/mecir_dinheiro_em_circulacao/versao/v1/odata/informacoes_diarias_com_categoria?$top=100&$orderby=Data%20desc&$format=json"
+
+requisicao = requests.get(link2)
+informacoes = requisicao.json()
+
+# COMMAND ----------
+
+# MAGIC %md ### TRANSFORMANDO A VARIVEL EM UM DATAFRAME
+# MAGIC
+
+# COMMAND ----------
+
+from pyspark.sql.types import *
+
+tabela = pd.DataFrame(informacoes["value"])
+
+tabela['Date_atu'] = pd.to_datetime(tabela['Data']).dt.date
+
+tabela = tabela.drop(columns=['Data'])
+
+display(tabela)
+
+# COMMAND ----------
+
+# MAGIC %md #### TRAZENDO A DATA MAIS RECENTE DA TABELA
+
+# COMMAND ----------
+
+from datetime import date, timedelta
+
+data_atual = date.today()
+td = timedelta(1)
+data_atual = data_atual - td
+print(data_atual) 
+
+# COMMAND ----------
+
+tabela2 = tabela.where(tabela.Date_atu == data_atual).dropna()
+
+display(tabela2)
+
+
+# COMMAND ----------
+
+# MAGIC %md ### SALVANDO OS DADOS NO DATA LAKE
+
+# COMMAND ----------
+
+account_url = "https://datalakedatamaster2.blob.core.windows.net"
+token = "Ojfw6PpbOeUfrmo9zn0lBnMBdutV4FHtL3y7slsR3XsB03TWue8yrTFtGAkAWknWg/j6rlHqTbW7+AStnfju7g=="
+container = "bacen/historico_circulacao"
+blobname = "tabela_{}.csv".format(data_atual)
+
+container_client = ContainerClient(account_url, container, credential=token)
+blob_client = container_client.get_blob_client(blob=blobname)
+
+
+pandablob.df_to_blob(tabela2,blob_client,overwrite=True)
